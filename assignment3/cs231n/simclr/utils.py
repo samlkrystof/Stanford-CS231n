8ce537c16a033e60a5b331a40e5f20f4f6aadb77
samlkrystof <87620237+samlkrystof@users.noreply.github.com>
2022-09-16 11:51:10 +0200

    utils.py completed

--- assignment3/cs231n/simclr/utils.py
+++ assignment3/cs231n/simclr/utils.py
@@ -36,7 +36,9 @@ def train(model, data_loader, train_optimizer, epoch, epochs, batch_size=32, tem
         # Run x_i and x_j through the model to get out_left, out_right.              #
         # Then compute the loss using simclr_loss_vectorized.                        #
         ##############################################################################
-        
+        _, out_left = model(x_i)
+        _, out_right = model(x_j)
+        loss = simclr_loss_vectorized(out_left, out_right, temperature)
         
         ##############################################################################
         #                               END OF YOUR CODE                             #
